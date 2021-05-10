355db0d2348f34049c9b9ee96e0a20c9d25d0533
Andrey <belov_andrey@inbox.ru>
2021-05-10 22:21:11 +0300

    Модуль 6 Задание 1

--- index.php
+++ index.php
@@ -8,23 +8,31 @@ setlocale(LC_ALL, 'ru_RU');
 $con = mysqli_connect("localhost", "mysql", "mysql", "readme", 3306);
 mysqli_set_charset($con, "utf8");
 
-$sql_posts_id_filter ="";
-if ($_GET['content_id']) {
-    $sql_posts_id_filter = "WHERE content_type.id = ".$_GET['content_id'];
-}
+$content_type_filter = "";
+if (is_numeric($_GET['content_id'])) {
+    $content_type_filter = "WHERE content_type.id = ?";
+} 
 
 $sql_posts_select = "SELECT post_header AS header, class AS type, post_content AS content, login AS user_name, avatar, post_date, link, image, post.id
                     FROM post
                     JOIN user ON post.user_id = user.id
                     JOIN content_type ON post.content_type = content_type.id
-                    $sql_posts_id_filter
+                    $content_type_filter
                     ORDER BY view_count;";
 
-$sql_content_types_select = "SELECT type, class, id
-                            FROM content_type;";
+$stmt = mysqli_prepare($con, $sql_posts_select);
 
-$sql_array = mysqli_query($con, $sql_posts_select);
-$posts_array = mysqli_fetch_all($sql_array,  MYSQLI_ASSOC);
+if ($content_type_filter) {
+	mysqli_stmt_bind_param($stmt, 'i', $_GET['content_id']);
+}
+
+
+mysqli_stmt_execute($stmt);
+$res = mysqli_stmt_get_result($stmt);
+$posts_array = mysqli_fetch_all($res,  MYSQLI_ASSOC);
+
+$sql_content_types_select = "SELECT type, class, id
+                             FROM content_type;";
 $sql_array = mysqli_query($con, $sql_content_types_select);
 $content_types = mysqli_fetch_all($sql_array,  MYSQLI_ASSOC);
 
--- post.php
+++ post.php
@@ -6,32 +6,48 @@ setlocale(LC_ALL, 'ru_RU');
 $con = mysqli_connect("localhost", "mysql", "mysql", "readme", 3306);
 mysqli_set_charset($con, "utf8");
 
-$post_id = $_GET['post_id'];
+
+if (is_numeric($_GET['post_id'])) {
+  $post_id = $_GET['post_id'];
+}
 
 $sql_post_content = "SELECT post_header AS header, class AS type, post_content AS content, login AS user_name, avatar, post_date, link, image, post.id, user_id
                     FROM post
                     JOIN user ON post.user_id = user.id
                     JOIN content_type ON post.content_type = content_type.id
-                    WHERE post.id = $post_id;";
+                    WHERE post.id = ?";
 
-$sql_array = mysqli_query($con, $sql_post_content);
 
-if (!mysqli_num_rows($sql_array)) {
+$stmt = mysqli_prepare($con, $sql_post_content);// пробная часть с подготовленными выражениями
+mysqli_stmt_bind_param($stmt, 'i', $post_id);
+mysqli_stmt_execute($stmt);
+$res = mysqli_stmt_get_result($stmt);
+
+if (!mysqli_num_rows($res)) {
   http_response_code(404);
+  exit;
 } else {
-  $post_content_array = mysqli_fetch_assoc($sql_array);
+  $post_content_array = mysqli_fetch_all($res,  MYSQLI_ASSOC);
   $user_id = $post_content_array['user_id'];
 
   $sql_subscribers_amount = "SELECT COUNT(*)
                              FROM subscription
-                             WHERE author_id = $user_id;";
-  $sql_array = mysqli_query($con, $sql_subscribers_amount);
+                             WHERE author_id = ?;";
+
+  $stmt = mysqli_prepare($con, $sql_subscribers_amount);
+  mysqli_stmt_bind_param($stmt, 'i', $user_id);
+  mysqli_stmt_execute($stmt);
+  $sql_array = mysqli_stmt_get_result($stmt);
   $subscribers_amount = mysqli_fetch_row($sql_array);
 
   $sql_publications_amount = "SELECT COUNT(*)
                               FROM post
-                              WHERE user_id = $user_id;";
-  $sql_array = mysqli_query($con, $sql_publications_amount);
+                              WHERE user_id = ?;";
+
+  $stmt = mysqli_prepare($con, $sql_publications_amount);
+  mysqli_stmt_bind_param($stmt, 'i', $user_id);
+  mysqli_stmt_execute($stmt);
+  $sql_array = mysqli_stmt_get_result($stmt);
   $publications_amount = mysqli_fetch_row($sql_array);
 
   $detailed_post_content = include_template('post_details_'.$post_content_array['type'].'_block.php', ['post_array' => $post_content_array]);
