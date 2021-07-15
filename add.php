78f948240598d01ba3189d19c3323fa64dcc427c
andrey-white <belov_andrey@inbox.ru>
2021-07-16 00:38:25 +0300

    Модуль 6 задание 2

---
+++ add.php
@@ -0,0 +1,131 @@
+<?php
+require_once('helpers.php');
+require_once('functions.php');
+date_default_timezone_set("Europe/Moscow");
+setlocale(LC_ALL, 'ru_RU');
+
+$con = mysqli_connect("localhost", "mysql", "mysql", "readme", 3306);
+mysqli_set_charset($con, "utf8");
+
+$sql_content_types_select = "SELECT type AS post_type, class AS post_class
+                             FROM content_type;";
+$content_types_array = mysqli_query($con, $sql_content_types_select);
+$content_types = mysqli_fetch_all($content_types_array,  MYSQLI_ASSOC);
+
+$active_form_type = 'text';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {     // если форма отправлена, а не первый раз открываем страницу
+$post_data = $_POST;
+$errors = [];   
+$type_of_post = $post_data['type_of_post'];
+$allowed_image_types = ['png', 'jpg', 'gif', 'image/gif', 'image/png', 'image/jpg'];
+
+
+$rules = [                                            
+    'post-header' => function($value) {
+        return not_empty_field($value);
+    },
+    'photo-link' => function($value) {
+        return photo_link_validation($value);
+    },
+    'post-tag' => function($value) {
+        return validate_tags($value);
+    },
+    'video-link' => function($value) {
+        return video_link_validation($value);
+    },
+    'post-text' => function($value) {
+        return not_empty_field($value);
+    },
+    'quote-author' => function($value) {
+        return not_empty_field($value);
+    }
+];
+
+
+foreach ($post_data as $key => $value) {    // проверяем отправленную форму на соответствие правилам
+    if (isset($rules[$key])) {
+        $rule = $rules[$key];
+        $errors[$key] = $rule($value);
+    }
+}
+
+$errors = array_filter($errors);
+
+if (!empty($_FILES['added-photo-file']['name'])) {               // работаем с файлом, проверяем , загружаем
+  $tmp_name = $_FILES['added-photo-file']['tmp_name'];
+  $path = 'uploads/'.$_FILES['added-photo-file']['name'];
+
+  $finfo = finfo_open(FILEINFO_MIME_TYPE);
+  $file_type = finfo_file($finfo, $tmp_name);
+  if (!in_array($file_type, $allowed_image_types)) {
+    $errors['photo-link'] = 'Загрузите картинку в допустимом формате';
+  }
+  else {
+    move_uploaded_file($tmp_name, $path);
+    $post_data['photo-link'] = $_FILES['added-photo-file']['name'];
+  }
+} elseif (empty($errors['photo-link'])) {                                                          // или скачиваем файл по ссылке
+  $link = $post_data['photo-link'];
+  $file_info = pathinfo($link);
+    if (!in_array($file_info['extension'], $allowed_image_types)) {
+      $errors['photo-link'] = 'Недопустимый формат изображения';
+    } elseif ($file = file_get_contents($link)) {
+      $file_path = "uploads/".$file_info['basename'];
+      file_put_contents($file_path, $file);
+      $post_data['photo-link'] = $file_path;
+      } else {
+        $errors['photo-link'] = "Не удалось скачать изображение";
+      }
+}
+  
+
+if (count($errors)) {      // если были ошибки, загружаем страницу и показываем ошибки
+
+  $page_content = include_template('post_add.php', ['content_type' => $content_types, 'errors' => $errors]);
+  print($page_content);
+
+} else {                     // если не было ошибок, добавляем записи в базу и переадресовываем на страницу поста
+
+  $sql_add_query = "INSERT INTO post (post_date, post_header, post_content, quote_author, image, video, link, view_count, user_id, content_type)
+               VALUES (NOW(), ?, ?, ?, ?, ?, ?, DEFAULT, 1, ?)";
+  $stmt = mysqli_prepare($con, $sql_add_query);
+    mysqli_stmt_bind_param($stmt, 'sssssss', $post_data['post-header'], $post_data['post-text'], $post_data['quote-author'], $post_data['photo-link'], $post_data['video-link'], $post_data['post-link'], $post_data['content-type']);
+    mysqli_stmt_execute($stmt);
+    $new_post_id = mysqli_insert_id($con);
+
+   $tags =  explode (' ', $post_data['post-tag']);
+
+   foreach ($tags as $tag_name) {
+   	$sql = "INSERT INTO hashtag (hashtag_name)
+               VALUES (?)";
+    $stmt = mysqli_prepare($con, $sql);
+   	mysqli_stmt_bind_param($stmt, 's', $tag_name);
+   	mysqli_stmt_execute($stmt);
+   	$hashtag_id = mysqli_insert_id($con);
+
+   	if (!$hashtag_id) {
+   		$sql = "SELECT id
+   		        FROM hashtag
+   		        WHERE hashtag_name = ?;"; // возможно надо like
+   		$stmt = mysqli_prepare($con, $sql);
+   		mysqli_stmt_bind_param($stmt, 's', $tag_name);
+   		mysqli_stmt_execute($stmt);
+   		$res = mysqli_stmt_get_result($stmt);
+    		$hashtag_id = mysqli_fetch_row($res);
+   	}
+
+   	$sql = "INSERT INTO tag_in_post (tag_id, post_id)
+                   VALUES ( ?, ?);";
+      $stmt = mysqli_prepare($con, $sql);
+      mysqli_stmt_bind_param($stmt, 'ii', $hashtag_id, $new_post_id);
+      mysqli_stmt_execute($stmt);
+   }
+   header("Location: post.php?post_id=" . $new_post_id);
+}
+
+}
+
+$page_content = include_template('post_add.php', ['content_type' => $content_types]);
+print($page_content);
+?>
---
+++ functions.php
@@ -0,0 +1,68 @@
+<?php
+
+function added_photo_validation ($value) {
+  if ($file_type !== 'image/gif' || $file_type !== 'image/png' || $file_type !== 'image/jpeg') {
+    return 'Пожалуйста, загрузите картинку в формате Gif, PNG или JPEG';
+  }
+  return null;
+}
+
+function video_link_validation ($value) {
+    if (empty($value)) {
+        return 'Адрес ссылки на видео должен быть заполнен';
+    }
+    if (!filter_var($value, FILTER_VALIDATE_URL)) {
+        return 'Пожалуйста, проверьте и введите корректный адрес ссылки';
+    }
+    return check_youtube_url($value);
+}
+
+
+function photo_link_validation ($value) {
+    if (empty($value) & !isset($_FILES['added-photo-file'])) {
+        return 'Адрес ссылки должен быть заполнен';
+    }
+    if (!filter_var($value, FILTER_VALIDATE_URL) & !isset($_FILES['added-photo-file'])) {
+        return 'Введите корректный адрес ссылки';
+    }
+
+    return null;
+}
+
+function not_empty_field($value) {
+    if (empty($value)) {
+        return 'Поле должно быть заполнено';
+    }
+    return null;
+}
+
+
+function validate_tags($value): ?string {             // функция валидации тегов
+    if (empty($value)) {
+        return 'Поле должно быть заполнено';
+    }
+
+    $tag_array = array_filter(explode(' ', $value));
+
+    foreach ($tag_array as $tag) {
+        if (!boolval(preg_match('/^[a-zA-Zа-яёА-ЯЁ0-9_]+$/u', $tag))) {
+            return 'Введите корректный тег';
+        }
+    }
+
+    return null;
+}
+
+function getPostVal($name) {
+    return $_POST[$name] ?? "";
+}
+
+function is_error_field ($value) {
+    if (empty($value)) {
+        return null;
+    } else {
+        return 'form__input-section--error';
+    }
+}
+
+?>
